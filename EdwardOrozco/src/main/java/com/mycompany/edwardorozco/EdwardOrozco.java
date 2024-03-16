e8555c878770cfcbe3057291d2e70be0d341a057
edward9108 <edwardorozco@unicomfacauca.edu.co>
2024-03-16 11:48:28 -0500

    1

---
+++ EdwardOrozco/src/main/java/com/mycompany/edwardorozco/EdwardOrozco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.edwardorozco;
+
+/**
+ *
+ * @author usuario
+ */
+public class EdwardOrozco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
